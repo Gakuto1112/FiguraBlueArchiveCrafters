bce63ed9fb991acbd2a005e592f0739cfdb05910
Gakuto1112 <gakuto131112@gmail.com>
2026-03-20 22:01:20 +0900

    引数の処理を追加

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -1,12 +1,19 @@
 import argparse
+from pathlib import Path
 
 from modules.paths import paths
 
 
-def build():
+# サムネイル画像を生成するかどうかのフラグ
+should_generate_thumbnails: bool = True
+
+# スクリプトが監視モードかどうかのフラグ
+is_observe_mode: bool = False
+
+def build() -> None:
 	pass
 
-def main():
+def main() -> None:
 	"""
 	エントリー関数
 	"""
@@ -20,6 +27,21 @@ def main():
 	parser.add_argument("--observe", "-o", action="store_true", help="Observes the source directory for changes and automatically rebuilds the affected avatars.")
 
 	args = parser.parse_args()
+	paths.source_dir = Path(args.src_dir)
+	paths.distribution_dir = Path(args.dist_dir)
+	global should_generate_thumbnails, is_observe_mode
+	should_generate_thumbnails = not args.skip_thumbnails
+	is_observe_mode = args.observe
+
+	target_avatars: list[str] = []
+	if args.character:
+		target = next((avatar for avatar in paths.get_avatar_names() if args.character in avatar), None)
+		if target is None:
+			raise ValueError(f"Specified character '{args.character}' does not exist in the character directory.")
+
+		target_avatars.append(target)
+	else:
+		target_avatars = list(paths.get_avatar_names())
 
 if __name__ == "__main__":
 	main()
