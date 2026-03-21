336750e9307dd40a44c9d272e744ade3a46d9078
Gakuto1112 <gakuto131112@gmail.com>
2026-03-21 13:27:56 +0900

    ロガーにデバッグレベルを追加

--- build_scripts/modules/logger.py
+++ build_scripts/modules/logger.py
@@ -6,6 +6,19 @@ class Logger:
 	標準出力を管理するユーティリティクラス
 	"""
 
+	should_print_debug_log: bool = True
+	"""
+	DEBUGレベルのログを出力するかどうかのフラグ
+	"""
+
+	def print_debug(self, message: str) -> None:
+		"""
+		DEBUGレベルのメッセージを標準出力する。
+		`should_print_debug_log`がFalseの場合は何も出力しない。
+		"""
+		if self.should_print_debug_log:
+			print(f"DEBUG: {message}")
+
 	def print_info(self, message: str) -> None:
 		"""
 		INFOレベルのメッセージを標準出力する。
