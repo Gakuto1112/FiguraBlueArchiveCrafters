3c588fce63ad184df44d2ea6630618ae2827e4de
Gakuto1112 <gakuto131112@gmail.com>
2026-03-22 00:08:18 +0900

    サムネイル画像に影を追加

--- build_scripts/modules/thumbnail_generator.py
+++ build_scripts/modules/thumbnail_generator.py
@@ -146,39 +146,43 @@ class ThumbnailGenerator:
 		# 背景とキャラクターのマスク画像
 		mask_image: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "trimming_mask.png").convert("L")
 
-		# レイヤー1: 背景
-		layer1: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L1_background.png").convert("RGBA")
-		layer1.putalpha(mask_image)
+		# レイヤー1: 影
+		layer1: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L1_shadow.png").convert("RGBA")
 		canvas.alpha_composite(layer1)
 
-		# レイヤー2: 白枠
-		canvas.alpha_composite(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L2_frame.png").convert("RGBA"))
+		# レイヤー2: 背景
+		layer2: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L2_background.png").convert("RGBA")
+		layer2.putalpha(mask_image)
+		canvas.alpha_composite(layer2)
 
-		# レイヤー3: キャラクター
-		if (layer3_path := paths.character_dir / avatar_name / "thumbnail.png").exists():
+		# レイヤー3: 白枠
+		canvas.alpha_composite(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L3_frame.png").convert("RGBA"))
+
+		# レイヤー4: キャラクター
+		if (layer4_path := paths.character_dir / avatar_name / "thumbnail.png").exists():
 			# キャラクター画像のトリミング
-			layer3: Image.Image = thumbnail_generator._open_image(layer3_path).convert("RGBA")
-			width, height = layer3.size
+			layer4: Image.Image = thumbnail_generator._open_image(layer4_path).convert("RGBA")
+			width, height = layer4.size
 			square_size = min(width, height)
-			layer3 = layer3.crop(((width - square_size) // 2, (height - square_size) // 2, (width + square_size) // 2, (height + square_size) // 2))
-			layer3 = layer3.resize((256, 256), resample=Image.Resampling.LANCZOS)
+			layer4 = layer4.crop(((width - square_size) // 2, (height - square_size) // 2, (width + square_size) // 2, (height + square_size) // 2))
+			layer4 = layer4.resize((256, 256), resample=Image.Resampling.LANCZOS)
 
 			# マスクキング
-			layer3.putalpha(ImageChops.multiply(mask_image, layer3.getchannel("A")))
-			canvas.alpha_composite(layer3)
+			layer4.putalpha(ImageChops.multiply(mask_image, layer4.getchannel("A")))
+			canvas.alpha_composite(layer4)
 		else:
 			logger.print_warning(f"Character thumbnail not found ({avatar_name}). Character layer will be skipped.")
 
-		# レイヤー4: 色付き枠
+		# レイヤー5: 色付き枠
 		palette: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "frame_colors.png").convert("RGBA")
-		layer4: Image.Image = Image.new("RGBA", (256, 256), cast(tuple[int, int, int, int], palette.getpixel((ThumbnailColorType[thumbnail_config["colorType"]].value, 0)))) # pyright: ignore[reportUnknownMemberType]
-		layer4.putalpha(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L4_colored_frame.png").convert("L"))
-		canvas.alpha_composite(layer4)
+		layer5: Image.Image = Image.new("RGBA", (256, 256), cast(tuple[int, int, int, int], palette.getpixel((ThumbnailColorType[thumbnail_config["colorType"]].value, 0)))) # pyright: ignore[reportUnknownMemberType]
+		layer5.putalpha(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L5_colored_frame.png").convert("L"))
+		canvas.alpha_composite(layer5)
 
-		# レイヤー5: ウィジェット
-		canvas.alpha_composite(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L5_widgets.png").convert("RGBA"))
+		# レイヤー6: ウィジェット
+		canvas.alpha_composite(thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "L6_widgets.png").convert("RGBA"))
 
-		# レイヤー6: アバターID
+		# レイヤー7: アバターID
 		sprite_sheet: Image.Image = thumbnail_generator._open_image(paths.root / "thumbnail_templates" / "char_sprites.png").convert("RGBA")
 
 		SPRITE_SIZE: tuple[int, int] = (7, 9) # スプライトシート内のスプライト1つのサイズ（x, y）
@@ -203,7 +207,7 @@ class ThumbnailGenerator:
 		CHAR_GAP : int = 1 # スプライト同士の間隔
 		ID_ANCHOR: tuple[int, int] = (12, 178) # サムネイル上でのアバタIDの貼り付け基点（サムネイル、ID共に左上が基準点）
 
-		layer6: Image.Image = Image.new("RGBA", (SPRITE_SIZE[0] * CHAR_SCALE * 3 + CHAR_GAP * 2, SPRITE_SIZE[1] * CHAR_SCALE), (0, 0, 0, 0))
+		layer7: Image.Image = Image.new("RGBA", (SPRITE_SIZE[0] * CHAR_SCALE * 3 + CHAR_GAP * 2, SPRITE_SIZE[1] * CHAR_SCALE), (0, 0, 0, 0))
 
 		for i, char in enumerate(avatar_name.split("_", 1)[0]):
 			if char not in SPRITE_MAP:
@@ -212,9 +216,9 @@ class ThumbnailGenerator:
 
 			sprite = sprite_sheet.crop((SPRITE_MAP[char][0] * SPRITE_SIZE[0], SPRITE_MAP[char][1] * SPRITE_SIZE[1], SPRITE_MAP[char][0] * SPRITE_SIZE[0] + SPRITE_SIZE[0], SPRITE_MAP[char][1] * SPRITE_SIZE[1] + SPRITE_SIZE[1]))
 			sprite = sprite.resize((SPRITE_SIZE[0] * CHAR_SCALE, SPRITE_SIZE[1] * CHAR_SCALE), Image.Resampling.NEAREST)
-			layer6.paste(sprite, ((SPRITE_SIZE[0] * CHAR_SCALE + CHAR_GAP) * i, 0))
+			layer7.paste(sprite, ((SPRITE_SIZE[0] * CHAR_SCALE + CHAR_GAP) * i, 0))
 
-		canvas.alpha_composite(layer6, ID_ANCHOR)
+		canvas.alpha_composite(layer7, ID_ANCHOR)
 
 		return canvas
 
