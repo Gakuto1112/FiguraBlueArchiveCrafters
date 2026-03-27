1ba5d5ac6662e0fe9258404f2f9d2fecef3ad9db
Gakuto1112 <gakuto131112@gmail.com>
2026-03-27 21:18:35 +0900

    モデルパーツエイリアスを作成

--- src/core/scripts/avatar.lua
+++ src/core/scripts/avatar.lua
@@ -3,5 +3,9 @@
 ---@type VanillaModel
 local VanillaModel = require("scripts.vanilla_model")
 
+---@type ModelAlias
+local ModelAlias = require("scripts.model_alias")
+
 -- モジュールの初期化
 VanillaModel.init()
+ModelAlias:init()
