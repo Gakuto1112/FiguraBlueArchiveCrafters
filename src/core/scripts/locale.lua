92313b565e35f295f5872ef60064ac7bb7d77b09
Gakuto1112 <gakuto131112@gmail.com>
2026-03-28 20:27:23 +0900

    キャッシュディレクトリの初期化機能を作成

--- src/core/scripts/locale.lua
+++ src/core/scripts/locale.lua
@@ -1,12 +1,16 @@
 
 
 ---@class (exact) Locale メッセージのローカライズを管理するクラス
+---@field package CACHE_DIR_ROOT string ロケールキャッシュディレクトリのルートパス
 ---@field package HARDCODED_LOCALES table<string, string> 外部からのロケール取得前に使用されるハードコードされた、最低限のローカライズメッセージ
 ---@field package activeLocale string 現在有効になっているロケール
 ---@field package locales table<string, table<string, string>> ローカライズされたテキストを格納するテーブル
 local Locale = {
+	CACHE_DIR_ROOT = "Gakuto1112/FiguraBlueArchiveCrafters/locale/";
+
 	HARDCODED_LOCALES = {
 		["message.locale.err_not_allowed"] = "There is not permission to use Figura File API or access Figura home directory! Cannot get locale data!";
+		["message.locale.err_io"] = "Failed to operate locale cache directory.";
 	};
 
 	activeLocale = "en_us";
@@ -15,8 +19,15 @@ local Locale = {
 	---初期化関数
 	---@param self Locale
 	init = function (self)
+		-- ロケールデータの初期化
 		self:initializeLocaleData()
+
+		-- FileAPIへのアクセス許可を確認
 		if self.checkAvailability() then
+			-- キャッシュディレクトリの初期化
+			if not file:exists(self.CACHE_DIR_ROOT) then
+				self:initializeLocaleDirectory()
+			end
 		else
 			print(self.HARDCODED_LOCALES["message.locale.err_not_allowed"])
 		end
@@ -36,6 +47,21 @@ local Locale = {
 	---@return boolean isAllowed キャッシュアクセスが許可されているかどうか
 	checkAvailability = function ()
 		return file:allowed() and file:isPathAllowed("")
+	end;
+
+	---ロケールのキャッシュディレクトリを初期化する。
+	---@param self Locale
+	initializeLocaleDirectory = function (self)
+		if self.checkAvailability() then
+			if file:exists(self.CACHE_DIR_ROOT) then
+				file:delete(self.CACHE_DIR_ROOT)
+			end
+			if not file:mkdirs(self.CACHE_DIR_ROOT) then
+				error(self.HARDCODED_LOCALES["message.locale.err_io"])
+			end
+		else
+			error(self.HARDCODED_LOCALES["message.locale.err_not_allowed"])
+		end
 	end;
 
 	---翻訳キーに対応するローカライズされたテキストを返す。
