cc800c9d967837d8384d369cce71376070b18d4c
Gakuto1112 <gakuto131112@gmail.com>
2026-04-04 15:25:50 +0900

    スカートフィールド内のフィールドを必須化

--- src/avatars/00a_base/scripts/blue_archive_character.lua
+++ src/avatars/00a_base/scripts/blue_archive_character.lua
@@ -190,7 +190,7 @@ local BlueArchiveCharacter = {
 	};
 
 	skirt = {
-
+		skirtModels = {};
 	};
 
 	gun = {
--- src/core/scripts/skirt.lua
+++ src/core/scripts/skirt.lua
@@ -2,7 +2,7 @@
 local Skirt = {
     ---初期化関数
     init = function ()
-        if BlueArchiveCharacter.skirt.skirtModels ~= nil and #BlueArchiveCharacter.skirt.skirtModels > 0 then
+        if #BlueArchiveCharacter.skirt.skirtModels > 0 then
             events.TICK:register(function ()
                 local isCrouching = player:isCrouching()
                 for _, skirtModel in ipairs(BlueArchiveCharacter.skirt.skirtModels) do
