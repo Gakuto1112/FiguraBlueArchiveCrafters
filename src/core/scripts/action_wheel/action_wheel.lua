2f724fa06435a328dffdc6e24503a694cc7d035a
Gakuto1112 <gakuto131112@gmail.com>
2026-04-05 14:19:58 +0900

    バリエーション衣装変更のアクションを作成

--- src/core/scripts/action_wheel/action_wheel.lua
+++ src/core/scripts/action_wheel/action_wheel.lua
@@ -7,34 +7,56 @@
 ---@field package mainPage Page アクションホイールのメインページのインスタンス
 ---@field package configPage Page アクションホイールのアバター設定ページのインスタンス
 local ActionWheel = {
-	mainPage = nil;
-	configPage = nil;
+	mainPage = action_wheel:newPage("main");
+	configPage = action_wheel:newPage("config");
 
     ---初期化関数
     ---@param self ActionWheel
     init = function (self)
 		if host:isHost() then
-			self.mainPage = action_wheel:newPage("main")
-			self.configPage = action_wheel:newPage("config")
-
 			action_wheel:setPage(self.mainPage)
 		end
 	end;
 
-	---アクションホイールのページにアクションを登録し、そのアクションのインスタンスを返す。
+	---アクションホイールのページにアクションを登録する。
 	---@param self ActionWheel
+	---@param action Action 登録対象のアクションのインスタンス
 	---@param target ActionWheel.Page アクションを登録するページ
 	---@param index? integer アクションのインデックス（1-8）。`nil`の場合は空いている最初のスロットに登録される。
-	---@return Action action アクションホイールに登録したアクションのインスタンス
-	setAction = function (self, target, index)
+	setAction = function (self, action, target, index)
 		if target == "MAIN" then
-			return self.mainPage:newAction(index)
+			self.mainPage:setAction(index or -1, action)
 		elseif target == "CONFIG" then
-			return self.configPage:newAction(index)
-		else
-			return nil
+			self.configPage:setAction(index or -1, action)
 		end
 	end;
+
+	---トグルアクションの雛形を生成して返す。
+	---@return Action toggleActionTemplate トグルアクションの雛形
+	getToggleAction = function ()
+		return action_wheel:newAction()
+			:setColor(0.67, 0, 0)
+			:setToggleColor(0, 0.67, 0)
+			:setHoverColor(1, 0.33, 0.33)
+	end;
+
+	---入力されたトグルアクションのトグル状態を返す。
+	---@param action Action トグル状態を取得したいアクションのインスタンス
+	---@return boolean state トグルアクションのトグル状態
+	getToggleActionState = function (action)
+		return action:getHoverColor() == vectors.vec3(0.33, 1, 0.33)
+	end;
+
+	---トグルアクションのホバーカラーを設定する。
+	---@param action Action 設定対象のトグルアクションのインスタンス
+	---@param state boolean トグルアクションのトグル状態
+	setActionToggleHoverColor = function (action, state)
+		if state then
+			action:setHoverColor(0.33, 1, 0.33)
+		else
+			action:setHoverColor(1, 0.33, 0.33)
+		end
+	end
 }
 
 return ActionWheel
--- src/core/scripts/costume.lua
+++ src/core/scripts/costume.lua
@@ -1,20 +1,64 @@
 ---@class (exact) Costume キャラクターのコスチュームを管理するクラス
+---@field package changeAltCostumeAction Action キャラクターのバリエーション衣装を切り替えるアクション
 local Costume = {
+	changeAltCostumeAction = nil;
+
 	---初期化関数
     ---@param self Costume
     init = function (self)
 		local isAltCostumeProcessed = false
 		if host:isHost() then
+			self.changeAltCostumeAction = ActionWheel:getToggleAction()
+				:setItem("minecraft:leather_chestplate")
+
 			if Config:loadConfig("PRIVATE", "costume.is_alt_costume", false) then
-				self.setAltCostume(true)
-				Config.syncConfigs["isAltCostume"] = true;
-				isAltCostumeProcessed = true
+				if BlueArchiveCharacter.costume.isAltCostumeEnabled then
+					self.setAltCostume(true)
+					self.changeAltCostumeAction:setToggled(true)
+					ActionWheel.setActionToggleHoverColor(self.changeAltCostumeAction, true)
+					Config.syncConfigs["isAltCostume"] = true
+					isAltCostumeProcessed = true
+				else
+					Config:saveConfig("PRIVATE", "costume.is_alt_costume", false)
+				end
 			end
+
+			if BlueArchiveCharacter.costume.isAltCostumeEnabled then
+				self.changeAltCostumeAction
+					:setOnToggle(function (_, action)
+						self.setAltCostume(true)
+						ActionWheel.setActionToggleHoverColor(action, true)
+						self:setChangeAltCostumeActionTitle()
+						Config:saveConfig("PRIVATE", "costume.is_alt_costume", true)
+					end)
+					:setOnUntoggle(function (_, action)
+						self.setAltCostume(false)
+						ActionWheel.setActionToggleHoverColor(action, false)
+						self:setChangeAltCostumeActionTitle()
+						Config:saveConfig("PRIVATE", "costume.is_alt_costume", false)
+					end)
+			else
+				self.changeAltCostumeAction
+					:setColor(0.16, 0.16, 0.16)
+					:setOnToggle(function (_, action)
+						action:setToggled(false)
+						print(Locale:getLocalizedText("action_wheel.main_page.change_alt_costume.unavailable"))
+						MiscUtils.playErrorSound()
+					end)
+			end
+			self:setChangeAltCostumeActionTitle()
+
+			ActionWheel:setAction(self.changeAltCostumeAction, "MAIN", 1)
 		end
+
 		if not isAltCostumeProcessed then
 			HeadBlock:generateHeadBlockModel()
 			Portrait:generatePortraitModel()
 		end
+
+		EventManager.events["ON_LOCALE_REFRESH"]:register(function ()
+			self:setChangeAltCostumeActionTitle()
+		end)
 
 		EventManager.events["ON_CONFIG_SYNC"]:register(function (configData)
 			if configData["isAltCostume"] then
@@ -22,6 +66,26 @@ local Costume = {
 			end
 		end)
     end;
+
+	---バリエーション衣装変更アクションのタイトルを更新する。
+	---@param self Costume
+	setChangeAltCostumeActionTitle = function (self)
+		local text = ""
+		if not BlueArchiveCharacter.costume.isAltCostumeEnabled then
+			text = Locale:getLocalizedText("text_format.color_dark_gray")
+		end
+		text = text .. Locale:getLocalizedText("action_wheel.main_page.change_alt_costume.title")
+		if BlueArchiveCharacter.costume.isAltCostumeEnabled then
+			if ActionWheel.getToggleActionState(self.changeAltCostumeAction) then
+				text = text .. " " .. Locale:getLocalizedText("text_format.color_green") .. Locale:getLocalizedText("action_wheel.action.toggle_on")
+			else
+				text = text .. " " .. Locale:getLocalizedText("text_format.color_red") .. Locale:getLocalizedText("action_wheel.action.toggle_off")
+			end
+		else
+			text = text .. Locale:getLocalizedText("action_wheel.action.toggle_off")
+		end
+		self.changeAltCostumeAction:setTitle(text)
+	end;
 
 	---バリエーション衣装(通常衣装と少し異なる衣装、以前の「別衣装」ではない)を設定する。
 	---@param isAlt boolean `false`: 通常衣装, `true`: バリエーション衣装
