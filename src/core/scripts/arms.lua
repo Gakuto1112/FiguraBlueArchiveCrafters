e6b5b9bbb8b599320d7b6f2edac189740bd9504c
Gakuto1112 <gakuto131112@gmail.com>
2026-04-12 18:07:24 +0900

    腕処理イベントの関数化

--- src/core/scripts/arms.lua
+++ src/core/scripts/arms.lua
@@ -83,45 +83,11 @@ local Arms = {
                 shouldPreventDefault = BlueArchiveCharacter.arms.callbacks.onAdditionalRightArmProcess(BlueArchiveCharacter, self.armState.right)
             end
             if not shouldPreventDefault then
-                if self.armState.right == "GUN_MAIN_HAND" then
-                    events.TICK:register(function ()
-                        if self.armState.right == "GUN_MAIN_HAND" then
-                            self:processArmWingCount()
-                            if player:isSwingingArm() and not player:isLeftHanded() then
-                                ModelAlias.alias.avatar.rightArm:setParentType("RightArm")
-                            else
-                                ModelAlias.alias.avatar.rightArm:setParentType("Body")
-                            end
-                            if player:getActiveItem().id == "minecraft:crossbow" then
-                                self:setArmState("CROSSBOW", "CROSSBOW")
-                            end
-                        end
-                    end, "right_arm_tick")
-                    events.RENDER:register(function (delta)
-                        local headRot = vanilla_model.HEAD:getOriginRot()
-                        ModelAlias.alias.avatar.rightArm:setRot(player:isSwingingArm() and not player:isLeftHanded() and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * 2.5 + 90 + (player:isCrouching() and 30 or 0), headRot.y, 0))
-                    end, "right_arm_render")
-                elseif self.armState.right == "GUN_OFF_HAND" then
-                    events.TICK:register(function ()
-                        self:processArmWingCount()
-                    end, "right_arm_tick")
-                    events.RENDER:register(function (delta, context)
-                        local headRot = vanilla_model.HEAD:getOriginRot()
-                        local isSwingingArm = player:isSwingingArm() and not player:isLeftHanded()
-                        ModelAlias.alias.avatar.rightArm:setParentType((isSwingingArm or context == "FIRST_PERSON") and "RightArm" or "Body")
-                        ModelAlias.alias.avatar.rightArm:setRot(isSwingingArm and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * 2.5 + 90 + (player:isCrouching() and 30 or 0), math.map((headRot.y + 180) % 360 - 180, -50, 50, -21, 78), 0))
-                    end, "right_arm_render")
-                elseif self.armState.right == "CROSSBOW" then
-                    events.TICK:register(function ()
-                        if player:getActiveItem().id ~= "minecraft:crossbow" and self.armState.right == "CROSSBOW" then
-                            if Gun.currentGunPosition == "RIGHT" then
-                                self:setArmState("GUN_MAIN_HAND", "GUN_OFF_HAND")
-                            elseif Gun.currentGunPosition == "LEFT" then
-                                self:setArmState("GUN_OFF_HAND", "GUN_MAIN_HAND")
-                            end
-                        end
-                    end, "right_arm_tick")
+                if self.armState.right == "GUN_OFF_HAND" then
+                    ModelAlias.alias.avatar.rightArm:setParentType("Body")
                 end
+                self:registerRightArmTickEvent(self.armState.right)
+                self:registerRightArmRenderEvent(self.armState.right)
             end
             self.armStatePrev.right = self.armState.right
         end
@@ -137,51 +103,121 @@ local Arms = {
                 BlueArchiveCharacter.arms.callbacks.onAdditionalLeftArmProcess(BlueArchiveCharacter, self.armState.left)
             end
             if not shouldPreventDefault then
-                if self.armState.left == "GUN_MAIN_HAND" then
-                    --銃を構えている際の、銃を構えている方の腕
-                    events.TICK:register(function ()
-                        if self.armState.left == "GUN_MAIN_HAND" then
-                            self:processArmWingCount()
-                            if player:isSwingingArm() and player:isLeftHanded() then
-                                ModelAlias.alias.avatar.leftArm:setParentType("LeftArm")
-                            else
-                                ModelAlias.alias.avatar.leftArm:setParentType("Body")
-                            end
-                            if player:getActiveItem().id == "minecraft:crossbow" then
-                                self:setArmState("CROSSBOW", "CROSSBOW")
-                            end
-                        end
-                    end, "left_arm_tick")
-                    events.RENDER:register(function (delta)
-                        local headRot = vanilla_model.HEAD:getOriginRot()
-                        ModelAlias.alias.avatar.leftArm:setRot(player:isSwingingArm() and player:isLeftHanded() and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * -2.5 + 90 + (player:isCrouching() and 30 or 0), headRot.y, 0))
-                    end, "left_arm_render")
-                elseif self.armState.left == "GUN_OFF_HAND" then
-                    --銃を構えている際の、銃を構えていない方の腕
+                if self.armState.left == "GUN_OFF_HAND" then
                     ModelAlias.alias.avatar.leftArm:setParentType("Body")
-                    events.TICK:register(function ()
-                        self:processArmWingCount()
-                    end, "left_arm_tick")
-                    events.RENDER:register(function (delta, context)
-                        local headRot = vanilla_model.HEAD:getOriginRot()
-                        local isSwingingArm = player:isSwingingArm() and player:isLeftHanded()
-                        ModelAlias.alias.avatar.leftArm:setParentType((isSwingingArm or context == "FIRST_PERSON") and "LeftArm" or "Body")
-                        ModelAlias.alias.avatar.leftArm:setRot(isSwingingArm and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * -2.5 + 90 + (player:isCrouching() and 30 or 0), math.map((headRot.y + 180) % 360 - 180, -50, 50, -78, 21), 0))
-                    end, "left_arm_render")
-                elseif self.armState.left == "CROSSBOW" then
-                    --クロスボウ装填中
-                    events.TICK:register(function ()
-                        if player:getActiveItem().id ~= "minecraft:crossbow" and self.armState.left == "CROSSBOW" then
-                            if Gun.currentGunPosition == "RIGHT" then
-                                self:setArmState("GUN_MAIN_HAND", "GUN_OFF_HAND")
-                            elseif Gun.currentGunPosition == "LEFT" then
-                                self:setArmState("GUN_OFF_HAND", "GUN_MAIN_HAND")
-                            end
-                        end
-                    end, "left_arm_tick")
                 end
+                self:registerLeftArmTickEvent(self.armState.left)
+                self:registerLeftArmRenderEvent(self.armState.left)
             end
             self.armStatePrev.left = self.armState.left
+        end
+    end;
+
+    ---右腕のティック処理を登録する。
+    ---@param self Arms
+    ---@param state Arms.BaseArmState|BlueArchiveCharacter.AdditionalArmState 腕の状態
+    registerRightArmTickEvent = function (self, state)
+        if state == "GUN_MAIN_HAND" then
+            events.TICK:register(function ()
+                if self.armState.right == "GUN_MAIN_HAND" then
+                    self:processArmWingCount()
+                    if player:isSwingingArm() and not player:isLeftHanded() then
+                        ModelAlias.alias.avatar.rightArm:setParentType("RightArm")
+                    else
+                        ModelAlias.alias.avatar.rightArm:setParentType("Body")
+                    end
+                    if player:getActiveItem().id == "minecraft:crossbow" then
+                        self:setArmState("CROSSBOW", "CROSSBOW")
+                    end
+                end
+            end, "right_arm_tick")
+        elseif state == "GUN_OFF_HAND" then
+            events.TICK:register(function ()
+                self:processArmWingCount()
+            end, "right_arm_tick")
+        elseif state == "CROSSBOW" then
+            events.TICK:register(function ()
+                if player:getActiveItem().id ~= "minecraft:crossbow" and self.armState.right == "CROSSBOW" then
+                    if Gun.currentGunPosition == "RIGHT" then
+                        self:setArmState("GUN_MAIN_HAND", "GUN_OFF_HAND")
+                    elseif Gun.currentGunPosition == "LEFT" then
+                        self:setArmState("GUN_OFF_HAND", "GUN_MAIN_HAND")
+                    end
+                end
+            end, "right_arm_tick")
+        end
+    end;
+
+    ---右腕のレンダー処理を登録する。
+    ---@param self Arms
+    ---@param state Arms.BaseArmState|BlueArchiveCharacter.AdditionalArmState 腕の状態
+    registerRightArmRenderEvent = function (self, state)
+        if state == "GUN_MAIN_HAND" then
+            events.RENDER:register(function (delta)
+                local headRot = vanilla_model.HEAD:getOriginRot()
+                ModelAlias.alias.avatar.rightArm:setRot(player:isSwingingArm() and not player:isLeftHanded() and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * 2.5 + 90 + (player:isCrouching() and 30 or 0), headRot.y, 0))
+            end, "right_arm_render")
+        elseif state == "GUN_OFF_HAND" then
+            events.RENDER:register(function (delta, context)
+                local headRot = vanilla_model.HEAD:getOriginRot()
+                local isSwingingArm = player:isSwingingArm() and not player:isLeftHanded()
+                ModelAlias.alias.avatar.rightArm:setParentType((isSwingingArm or context == "FIRST_PERSON") and "RightArm" or "Body")
+                ModelAlias.alias.avatar.rightArm:setRot(isSwingingArm and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * 2.5 + 90 + (player:isCrouching() and 30 or 0), math.map((headRot.y + 180) % 360 - 180, -50, 50, -21, 78), 0))
+            end, "right_arm_render")
+        end
+    end;
+
+    ---左腕のティック処理を登録する。
+    ---@param self Arms
+    ---@param state Arms.BaseArmState|BlueArchiveCharacter.AdditionalArmState 腕の状態
+    registerLeftArmTickEvent = function (self, state)
+        if state == "GUN_MAIN_HAND" then
+            events.TICK:register(function ()
+                if self.armState.left == "GUN_MAIN_HAND" then
+                    self:processArmWingCount()
+                    if player:isSwingingArm() and player:isLeftHanded() then
+                        ModelAlias.alias.avatar.leftArm:setParentType("LeftArm")
+                    else
+                        ModelAlias.alias.avatar.leftArm:setParentType("Body")
+                    end
+                    if player:getActiveItem().id == "minecraft:crossbow" then
+                        self:setArmState("CROSSBOW", "CROSSBOW")
+                    end
+                end
+            end, "left_arm_tick")
+        elseif state == "GUN_OFF_HAND" then
+            events.TICK:register(function ()
+                self:processArmWingCount()
+            end, "left_arm_tick")
+        elseif state == "CROSSBOW" then
+            events.TICK:register(function ()
+                if player:getActiveItem().id ~= "minecraft:crossbow" and self.armState.left == "CROSSBOW" then
+                    if Gun.currentGunPosition == "RIGHT" then
+                        self:setArmState("GUN_MAIN_HAND", "GUN_OFF_HAND")
+                    elseif Gun.currentGunPosition == "LEFT" then
+                        self:setArmState("GUN_OFF_HAND", "GUN_MAIN_HAND")
+                    end
+                end
+            end, "left_arm_tick")
+        end
+    end;
+
+    ---左腕のレンダー処理を登録する。
+    ---@param self Arms
+    ---@param state Arms.BaseArmState|BlueArchiveCharacter.AdditionalArmState 腕の状態
+    registerLeftArmRenderEvent = function (self, state)
+        if state == "GUN_MAIN_HAND" then
+            events.RENDER:register(function (delta)
+                local headRot = vanilla_model.HEAD:getOriginRot()
+                ModelAlias.alias.avatar.leftArm:setRot(player:isSwingingArm() and player:isLeftHanded() and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * -2.5 + 90 + (player:isCrouching() and 30 or 0), headRot.y, 0))
+            end, "left_arm_render")
+        elseif state == "GUN_OFF_HAND" then
+            events.RENDER:register(function (delta, context)
+                local headRot = vanilla_model.HEAD:getOriginRot()
+                local isSwingingArm = player:isSwingingArm() and player:isLeftHanded()
+                ModelAlias.alias.avatar.leftArm:setParentType((isSwingingArm or context == "FIRST_PERSON") and "LeftArm" or "Body")
+                ModelAlias.alias.avatar.leftArm:setRot(isSwingingArm and vectors.vec3() or vectors.vec3(headRot.x + math.sin((self.swingCount + delta) / 100 * math.pi * 2) * -2.5 + 90 + (player:isCrouching() and 30 or 0), math.map((headRot.y + 180) % 360 - 180, -50, 50, -78, 21), 0))
+            end, "left_arm_render")
         end
     end;
 }
