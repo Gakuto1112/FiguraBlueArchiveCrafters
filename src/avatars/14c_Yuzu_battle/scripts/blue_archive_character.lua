f808c61f98386ef47013d33d97fc0a014575b8c8
Gakuto1112 <gakuto131112@gmail.com>
2026-05-31 16:12:42 +0900

    Exスキルのディスプレイの文字を追加

--- src/avatars/14c_Yuzu_battle/scripts/blue_archive_character.lua
+++ src/avatars/14c_Yuzu_battle/scripts/blue_archive_character.lua
@@ -397,6 +397,11 @@ local BlueArchiveCharacter = {
 
 			callbacks = {
 				onPreAnimation = function (self)
+					if not self.exSkill.primary.isInitialized then
+						models.models.ex_skill_1.CockpitFront.CockpitScreen1.TextAnchor:setPrimaryRenderType("CUTOUT")
+						models.models.ex_skill_1.CockpitFront.CockpitScreen1.TextAnchor:newText("ex_skill_1_screen_1_text"):setText("§b§kcanning...\n\nScan complete\nThe enemy is very strong.\n\nBut, don't worry.\nYou can defeat it."):setPos(0, 3.8, 0):setRot(0, 0, 0):setScale(0.03, 0.03, 0.03):setWidth(100)
+						self.exSkill.primary.isInitialized = true
+					end
 					FaceParts:setEmotion("CIRCLE", "CIRCLE_TEAR", "SHOCK", 26, true)
 					ModelAlias.alias.avatar.leftEye:setPos(0, -0.5, 0)
 					ModelAlias.alias.avatar.head.FearEffect:setVisible(true)
@@ -480,6 +485,10 @@ local BlueArchiveCharacter = {
 					end
 				end;
 			};
+
+			---このExスキルの初期化処理が行われたかどうか。
+			---@type boolean
+			isInitialized = false;
 
 			---Exスキルのために調整したFOV基準値
 			---@type number
