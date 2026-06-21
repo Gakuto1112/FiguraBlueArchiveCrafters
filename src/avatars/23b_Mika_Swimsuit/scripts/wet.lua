a35941c8eca7befd1f9634d65e46052504059c4a
Gakuto1112 <gakuto131112@gmail.com>
2026-06-21 22:39:52 +0900

    濡れカウンターのロジックと水滴表示のマスタースイッチを作成

--- src/avatars/23b_Mika_Swimsuit/scripts/wet.lua
+++ src/avatars/23b_Mika_Swimsuit/scripts/wet.lua
@@ -1,7 +1,14 @@
 ---@class (exact) Wet 水濡れをシミュレートし、髪の水滴などを制御するクラス
+---@field package MAX_WET_COUNT integer 濡れカウンターの最大値
 ---@field package waterDrops {model: ModelPart, sortValue: integer}[] 水滴のモデルパーツを格納するテーブル
+---@field package wetCount integer 水濡れの時間を測るカウンター
+---@field package isDropMasterVisible boolean 水滴モデル全体が見える状態かどうか（個別にオフになっている可能性もある）
 local Wet = {
+	MAX_WET_COUNT = 2400;
+
 	waterDrops = {};
+	wetCount = 0;
+	isDropMasterVisible = false;
 
 	---水滴モデルを`waterDrops`テーブルに格納する。
 	---@param self Wet
@@ -19,7 +26,18 @@ local Wet = {
 		for _, waterDrop in ipairs(self.waterDrops) do
 			waterDrop.model:setPrimaryRenderType("CUTOUT_EMISSIVE_SOLID")
 			waterDrop.model:setOpacity(0.05)
+			waterDrop.model:setVisible(false)
 		end
+	end;
+
+	---`waterDrops`テーブル内の全ての水滴モデルパーツの可視性を切り替える。
+	---@param self Wet
+	---@param value boolean 水滴モデルを表示するかどうか。
+	setDropsVisibleAll = function (self, value)
+		for _, waterDrop in ipairs(self.waterDrops) do
+			waterDrop.model:setVisible(value)
+		end
+		self.isDropMasterVisible = value
 	end;
 
 	---初期化関数
@@ -27,6 +45,27 @@ local Wet = {
 	init = function (self)
 		self:insertWaterDropModels()
 		self:initializeWaterDropModels()
+
+		events.TICK:register(function ()
+			if not client:isPaused() then
+				if player:isInWater() then
+					self.wetCount = self.MAX_WET_COUNT
+					self.nextDropCount = 0;
+
+					local isUnderwater = player:isUnderwater()
+					if isUnderwater == self.isDropMasterVisible then
+						self:setDropsVisibleAll(not isUnderwater)
+					end
+				else
+					if self.wetCount == self.MAX_WET_COUNT then
+					elseif self.wetCount == 0 and self.isDropMasterVisible then
+						self.nextDropCount = 0;
+						self:setDropsVisibleAll(false)
+					end
+					self.wetCount = math.max(self.wetCount - 1, 0)
+				end
+			end
+		end)
 	end;
 }
 
