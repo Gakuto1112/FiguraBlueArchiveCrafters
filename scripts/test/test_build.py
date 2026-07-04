778e25e101d627ab92c0d05cfed35cf9ebe585aa
Gakuto1112 <gakuto131112@gmail.com>
2026-07-04 16:24:46 +0900

    ビルドテストの出力を修正

--- scripts/test/test_build.py
+++ scripts/test/test_build.py
@@ -1,18 +1,22 @@
 import subprocess
+import sys
 import unittest
 
 from common_modules.base_path import base_path
 
 class TestBuild(unittest.TestCase):
-	def test_build(self) -> None:
-		"""
-		アバターのビルドが成功するかテストする。
-		"""
+    def test_build(self) -> None:
+        """
+        アバターのビルドが成功するかテストする。
+        """
 
-		try:
-			subprocess.run(["uv", "run", "-m", "build.build"], cwd=(base_path.root / "scripts"),check=True)
-		except:
-			self.fail("Failed to build avatars.")
+        try:
+            result = subprocess.run(["uv", "run", "-m", "build.build"], cwd=(base_path.root / "scripts"), check=True, capture_output=True, text=True)
+            print(result.stdout, end="")
+        except subprocess.CalledProcessError as error:
+            print(error.stdout, end="")
+            print(error.stderr, end="", file=sys.stdout)
+            self.fail(f"Failed to build avatars.\n{error.stderr}")
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
