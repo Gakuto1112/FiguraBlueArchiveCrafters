7e49c1cab82d5c3bd11ed5ce483ebac53d580192
Gakuto1112 <gakuto131112@gmail.com>
2026-07-12 22:07:23 +0900

    既存のバージョン比較関数を削除

--- src/core/scripts/utils/string_utils.lua
+++ src/core/scripts/utils/string_utils.lua
@@ -20,24 +20,6 @@ local StringUtils = {
         return result
     end;
 
-    ---2つのバージョン文字列を比較し、新しい方を返す。
-    ---@param version1 string 比較するバージョン文字列1
-    ---@param version2 string 比較するバージョン文字列2
-    ---@return string|nil newerVersion 新しい方のバージョン文字列。比較不可能だった場合はnilを返す。
-    compareVersions = function (version1, version2)
-        local major1, minor1, patch1 = version1:match("^v?(%d+)%.(%d+)%.?(%d*)")
-        local major2, minor2, patch2 = version2:match("^v?(%d+)%.(%d+)%.?(%d*)")
-        major1 = tonumber(major1)
-        minor1 = tonumber(minor1)
-        patch1 = patch1 ~= nil and tonumber(patch1) or 0
-        major2 = tonumber(major2)
-        minor2 = tonumber(minor2)
-        patch2 = patch2 ~= nil and tonumber(patch2) or 0
-        if major1 ~= nil and minor1 ~= nil and patch1 ~= nil and major2 ~= nil and minor2 ~= nil and patch2 ~= nil then
-            return (major1 > major2 or (major1 == major2 and minor1 > minor2) or (major1 == major2 and minor1 == minor2 and patch1 > patch2)) and version1 or version2
-        end
-    end;
-
     ---指定されたバージョン文字列が、比較対象のバージョン文字列より新しいものか比較する。
     ---@param targetVersion string 比較対象のバージョン文字列
     ---@param comparedVersion string 比較するバージョン文字列
